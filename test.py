6a08cfa432d69be853c128c2c925230480921f9d
BlueSkyBeijing <chenjunlong@kingsoft.com>
2021-05-04 18:46:45 +0800

    test tensorflow

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import tensorflow as tf
+
 #print tensorflow version
 print(tf.__version__)
+
+tf.compat.v1.disable_eager_execution()
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.compat.v1.Session()
+
+print(sess.run(hello))
